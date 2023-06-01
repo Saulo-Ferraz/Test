47955e37a024486269ac8da8e793e72b3cecb701
Saulo Ferraz <ferrazsaulo1@gmail.com>
2023-06-01 01:20:11 -0300

    DTO

--- demo/src/main/java/com/example/Projeto/dto/PlaylistDTO.java
+++ demo/src/main/java/com/example/Projeto/dto/PlaylistDTO.java
@@ -1,4 +1,18 @@
 package com.example.Projeto.dto;
 
+import java.time.LocalDate;
+import com.example.Projeto.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class PlaylistDTO {
+	private Long id;
+	private String nome;
+	private LocalDate data;
+	private User user;
 }
--- demo/src/main/java/com/example/Projeto/dto/PostDTO.java
+++ demo/src/main/java/com/example/Projeto/dto/PostDTO.java
@@ -1,4 +1,21 @@
 package com.example.Projeto.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+import com.example.Projeto.model.User;
+
+import lombok.AllArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class PostDTO {
+	private Long Id;
+	private String texto;
+	private LocalDate data;
+	private User user;
 }
